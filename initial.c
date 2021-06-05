dadeddadd56bc87683aa7d125c1b3b0f50908110
AlaaShatat <73889806+AlaaShatat@users.noreply.github.com>
2021-06-05 09:54:33 +0200

    PORT F initial

--- initial.c
+++ initial.c
@@ -29,4 +29,17 @@ void init()
 	GPIO_PORTB_PUR_R = 0; // no pull up resistors are needed
 	//GPIO_PORTB_LOCK_R = 0x4C4F434B; //  lock port D
 	GPIO_PORTB_CR_R |= 0xFF; // enable commiit to the 8 bins
+		
+			// PORT F initial
+		SYSCTL_RCGCGPIO_R |= 0x20; // port B select
+	//delay = 1;
+	while((SYSCTL_PRGPIO_R & 0x20) == 0){}; //delay until the portF is granted
+	GPIO_PORTF_AFSEL_R = 0; // alternate function select to GPIO
+	GPIO_PORTF_PCTL_R = 0;
+	GPIO_PORTF_AMSEL_R = 0; // no analog
+	GPIO_PORTF_DIR_R  = 0x0E; // LED as output
+	GPIO_PORTF_DEN_R = 0x1F; // all pins are digital output 
+	GPIO_PORTF_PUR_R = 0x11; // no pull up resistors are needed
+	GPIO_PORTF_LOCK_R = 0x4C4F434B; //  lock port D
+	GPIO_PORTF_CR_R |= 0x1F; // enable commiit to the 8 bins
 }
