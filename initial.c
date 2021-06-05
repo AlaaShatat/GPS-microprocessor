bf443b3e8374c2c21b18e102b8d3e9fdbd3d24da
Alaa-Ibrahim-Amer <69719564+Alaa-Ibrahim-Amer@users.noreply.github.com>
2021-06-05 15:44:45 +0200

    unlock port E

--- initial.c
+++ initial.c
@@ -4,7 +4,7 @@ void init()
 {
 	volatile unsigned long delay;
 			// PORT A initial
-		SYSCTL_RCGCGPIO_R |= 0x01; // port B select
+		SYSCTL_RCGCGPIO_R |= 0x01; // port A select
 	//delay = 1;
 	while((SYSCTL_PRGPIO_R & 0x01) == 0){}; //delay until the portA is granted
 	GPIO_PORTA_AFSEL_R = 0xFF; // alternate function select to GPIO
@@ -13,7 +13,7 @@ void init()
 	GPIO_PORTA_DIR_R  = 0xFF; // UART
 	GPIO_PORTA_DEN_R = 0xFF; // all pins are digital enabled
 	GPIO_PORTA_PUR_R = 0; // no pull up resistors are needed
-	//GPIO_PORTA_LOCK_R = 0x4C4F434B; //  lock port D
+	//GPIO_PORTA_LOCK_R = 0x4C4F434B; //  lock port A
 	GPIO_PORTA_CR_R |= 0xFF; // enable commiit to the 8 bins
 		
 		
@@ -27,12 +27,12 @@ void init()
 	GPIO_PORTB_DIR_R  = 0xFF; // all pins are output
 	GPIO_PORTB_DEN_R = 0xFF; // all pins are digital output 
 	GPIO_PORTB_PUR_R = 0; // no pull up resistors are needed
-	//GPIO_PORTB_LOCK_R = 0x4C4F434B; //  lock port D
+	//GPIO_PORTB_LOCK_R = 0x4C4F434B; //  lock port B
 	GPIO_PORTB_CR_R |= 0xFF; // enable commiit to the 8 bins
 		
 			// initialize port E
 	
-	SYSCTL_RCGCGPIO_R |= 0x10; // port D select
+	SYSCTL_RCGCGPIO_R |= 0x10; // port E select
 	//delay = 1;
 	while((SYSCTL_PRGPIO_R & 0x09) == 0){}; //delay until the portE is granted
 	GPIO_PORTE_AFSEL_R = 0; // alternate function select to GPIO
@@ -41,7 +41,7 @@ void init()
 	GPIO_PORTE_DIR_R  = 0xFF; // all pins are output
 	GPIO_PORTE_DEN_R = 0xFF; // all pins are digital output 
 	GPIO_PORTE_PUR_R = 0; // no pull up resistors are needed
-	GPIO_PORTE_LOCK_R = 0x4C4F434B; //  lock port E
+	//GPIO_PORTE_LOCK_R = 0x4C4F434B; //  lock port E
 	GPIO_PORTE_CR_R |= 0xFF; // enable commiit to the 8 bins
 			
 		
@@ -55,6 +55,6 @@ void init()
 	GPIO_PORTF_DIR_R  = 0x0E; // LED as output
 	GPIO_PORTF_DEN_R = 0x1F; // all pins are digital output 
 	GPIO_PORTF_PUR_R = 0x11; // no pull up resistors are needed
-	GPIO_PORTF_LOCK_R = 0x4C4F434B; //  lock port D
+	GPIO_PORTF_LOCK_R = 0x4C4F434B; //  lock portf
 	GPIO_PORTF_CR_R |= 0x1F; // enable commiit to the 8 bins
 }
