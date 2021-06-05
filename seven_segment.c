3b588e96e3570eefa8cf5efd6d430f8e01b1acd4
Nahla-Mustafa99 <nahla0019@gmail.com>
2021-06-05 20:20:41 +0200

    Implement_7segment_Display
    try  the code on Tiva

--- seven_segment.c
+++ seven_segment.c
@@ -1,8 +1,8 @@
 #include "stdint.h"
 #include "math.h"
-#include "D:\project\tm4c123gh6pm.h"
-
-
+#include "tm4c123gh6pm.h"
+#include "initial.h"
+void SystemInit(){}
   int test = 0x0000  ;
   int hundreds = 0x00;
 	int tens = 0x00;
@@ -19,15 +19,26 @@ void segment_output(long total_distance)
   tens = floor(number/10);
 	number = number %10; 
 	units = number;
+		
+	test=hundreds << 5;
+  GPIO_PORTA_DATA_R |= test  ; // hudreds :limit 700 A5 ->A7
+	test=tens << 1;
+	GPIO_PORTE_DATA_R |= test;//tens E1->EE4
+	GPIO_PORTD_DATA_R |=  units;//units  D0->D3
+	//a2a3a4a5a6a7
+	//e4e5e3e2e1
+  //GPIO_PORTE_DATA_R =  test;
+//	GPIO_PORTF_DATA_R = GPIO_PORTD_DATA_R;
+	//
 	
-	test = tens ; 
-	test = test << 4 ;
-	test = test | units ;
+	//''test = tens ; 
+	//''test = test << 4 ;
+	//''test = test | units ;
 	
 	}
 	
-  GPIO_PORTD_DATA_R = test ; 
-  GPIO_PORTE_DATA_R = hundreds;
+  //''GPIO_PORTD_DATA_R = test ; 
+  //''GPIO_PORTE_DATA_R = hundreds;
 	
 }
 
@@ -38,6 +49,7 @@ int main ()
 	init();
 	total_distance = 128;
   segment_output(total_distance);
+	
 	}
 }
 
