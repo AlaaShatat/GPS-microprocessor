986afb67a261b78ffb3041202051463a9b0180f2
Alaa-Ibrahim-Amer <69719564+Alaa-Ibrahim-Amer@users.noreply.github.com>
2021-06-06 23:20:41 +0200

    final edits

--- seven_segment.c
+++ seven_segment.c
@@ -1,12 +1,6 @@
 #include "stdint.h"
 #include "math.h"
 
-  int test = 0x0000  ;
-  int hundreds = 0x00;
-	int tens = 0x00;
-	int units = 0x00  ;	
-	long number= 0x00;
-  long total_distance = 0x0000 ;
 void segment_output(long total_distance)
 {
 	number = ceil(total_distance);
@@ -19,10 +13,11 @@ void segment_output(long total_distance)
 	units = number;
 		
 	test=hundreds << 5;
+		GPIO_PORTA_DATA_R = GPIO_PORTA_DATA_R &0x0F ;
   GPIO_PORTA_DATA_R |= test  ; // hudreds :limit 700 A5 ->A7
 	test=tens << 1;
-	GPIO_PORTE_DATA_R |= test;//tens E1->EE4
-	GPIO_PORTD_DATA_R |=  units;//units  D0->D3
+	GPIO_PORTE_DATA_R = test;//tens E1->EE4
+	GPIO_PORTD_DATA_R =  units;//units  D0->D3
 
 	}
 	
