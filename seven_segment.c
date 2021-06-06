7c705709440cb6916a8f876a5898d35e610aac57
Alaa-Ibrahim-Amer <69719564+Alaa-Ibrahim-Amer@users.noreply.github.com>
2021-06-06 23:12:37 +0200

    final edits

--- seven_segment.c
+++ seven_segment.c
@@ -13,14 +13,13 @@ void segment_output(long total_distance)
 	units = number;
 		
 	test=hundreds << 5;
+	GPIO_PORTA_DATA_R = GPIO_PORTA_DATA_R &0x0F ;
   GPIO_PORTA_DATA_R |= test  ; // hudreds :limit 700 A5 ->A7
 	test=tens << 1;
-	GPIO_PORTE_DATA_R |= test;//tens E1->EE4
-	GPIO_PORTD_DATA_R |=  units;//units  D0->D3
+	GPIO_PORTE_DATA_R = test;//tens E1->EE4
+	GPIO_PORTD_DATA_R =  units;//units  D0->D3
 	
 	}
-	
-
 	
 }
 
