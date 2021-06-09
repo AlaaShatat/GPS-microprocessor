9a0a122d45bb2fd9846a075cbccb2188a8a2413f
ayaatef223 <75602831+ayaatef223@users.noreply.github.com>
2021-06-09 15:30:25 +0200

    One Second Delay

--- initial.c
+++ initial.c
@@ -9,7 +9,17 @@ NVIC_ST_RELOAD_R =1600-1;
 NVIC_ST_CURRENT_R =0;
 NVIC_ST_CTRL_R =5 ;
 }
+void One_Second_Delay(uint32_t time)
+{ while (time){
+    NVIC_ST_CTRL_R = 0;            /* disable SysTick during setup */
+    NVIC_ST_RELOAD_R = 15999999;    /* Reload Value goes here */
+    NVIC_ST_CTRL_R |= 0x5;          /* enable SysTick with core clock */
+    while( (NVIC_ST_CTRL_R & (1<<16) ) == 0)
+        ;                           /* Monitoring bit 16 to be set */
+    NVIC_ST_CTRL_R = 0;             /* Disabling SysTick Timer */
+time -- ; 
 
+}}
 
 
 void init()
