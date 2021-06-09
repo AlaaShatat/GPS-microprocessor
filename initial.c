77108db04cba3ef770230c95aa97acdc10b94bdf
ayaatef223 <75602831+ayaatef223@users.noreply.github.com>
2021-06-09 15:33:30 +0200

    read uart0

--- initial.c
+++ initial.c
@@ -139,6 +139,10 @@ uint8_t UART0_Available(void){
 	return ((UART0_FR_R&UART_FR_RXFE) == UART_FR_RXFE) ? 0 : 1;
 }
 
+uint8_t UART0_Read(void){
+	while(UART0_Available() != 1);
+	return (uint8_t)(UART0_DR_R&0xFF);
+}
 
 
 
